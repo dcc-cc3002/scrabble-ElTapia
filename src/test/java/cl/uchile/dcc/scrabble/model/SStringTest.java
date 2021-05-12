360c745ea15746b1e25a4fc4a160130a7806ab1e
ElTapias <sebastian.tapia.st@gmail.com>
2021-05-11 20:16:06 -0400

    Add random tests

--- src/test/java/cl/uchile/dcc/scrabble/model/SStringTest.java
+++ src/test/java/cl/uchile/dcc/scrabble/model/SStringTest.java
@@ -1,37 +1,50 @@
 package cl.uchile.dcc.scrabble.model;
 
+import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
+
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class SStringTest {
     private SString sString;
-    private String testString = "Test String";
+    private String testString;
+    private int seed;
+    private Random rng;
 
     @BeforeEach
     void setUp(){
+        seed = new Random().nextInt();
+        rng = new Random(seed);
+        int strSize= rng.nextInt(50);
+        testString = RandomStringUtils.random(strSize, 0, Character.MAX_CODE_POINT,
+                true, true, null, rng);
         sString = new SString(testString);
     }
 
-    @Test
+    @RepeatedTest(50)
     void constructorTest(){
+        assertEquals(testString, sString.toString(), "String don't match. Seed" + seed);
+
         var expectedSString = new SString(testString);
 
-        assertEquals(expectedSString, sString);
-        assertEquals(expectedSString.hashCode(), sString.hashCode());
+        assertEquals(expectedSString, sString, "SString don't match. Seed" + seed);
+        assertEquals(expectedSString.hashCode(), sString.hashCode(), "Hashcode don't match. Seed" + seed);
 
-        var differentSString = new SString("Other string");
-        assertNotEquals(differentSString, sString);
-    }
-    @Test
-    void toStringTest(){
-        String expectedString = "Test String";
-        String differentString = "Other string";
+        String differentString;
 
-        assertEquals(expectedString, sString.toString());
-        assertNotEquals(differentString, sString.toString());
+        do {
+            differentString = RandomStringUtils.random(rng.nextInt(50), 0,
+                    Character.MAX_CODE_POINT, true, true, null, rng);
+        } while (differentString.equals(testString));
 
+        assertNotEquals(differentString, sString.toString(), "String match. Seed" + seed);
+
+        var differentSString = new SString(differentString);
+        assertNotEquals(differentSString, sString, "SString match. Seed" + seed);
     }
 }
