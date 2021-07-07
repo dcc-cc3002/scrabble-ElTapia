5a9a283785a7247548077a214a52049f78382cca
ElTapias <sebastian.tapia.st@gmail.com>
2021-07-06 21:13:43 -0400

    Create consString tests and implement methods

--- src/main/java/cl/uchile/dcc/scrabble/model/AST/Constants/ConsString.java
+++ src/main/java/cl/uchile/dcc/scrabble/model/AST/Constants/ConsString.java
@@ -2,17 +2,33 @@ package cl.uchile.dcc.scrabble.model.AST.Constants;
 
 import cl.uchile.dcc.scrabble.model.AST.*;
 import cl.uchile.dcc.scrabble.model.AST.Wrapper.*;
+import cl.uchile.dcc.scrabble.model.Abstract.AbstractType;
 import cl.uchile.dcc.scrabble.model.Types.*;
+import java.util.Objects;
 
 //TODO: Agregar factories para facilitar constructor
+//COMPLETE: Arreglar casteo a suma
 
+/**
+ * Constant String that represents a leaf of the AST. It wraps a Scrabble type.
+ */
 public class ConsString implements Constant, IOperation {
   private final SString consString;
 
+  /**
+   * Constructor
+   * @param string String
+   */
   public ConsString(String string){
     consString = new SString(string);
   }
 
+  /**
+   * Add this constant string to other constant.
+   * @param otherCons Constant
+   * @return Constant
+   */
+  @Override
   public Constant add(Constant otherCons){
     return otherCons.addConsStr(this);
   }
@@ -24,6 +40,38 @@ public class ConsString implements Constant, IOperation {
   @Override
   public SString getSType() {
     return new SString(this.consString.toString());
+  }
+
+  /**
+   * String representation of a constant
+   * @return String
+   */
+  @Override
+  public String toString(){
+    return this.consString.toString();
+  }
+
+  /**
+   * Get hashcode of type
+   * @return int
+   */
+  @Override
+  public int hashCode(){
+    return Objects.hash(AbstractType.class);
+  }
+
+  /**
+   * Compares an object with a constan
+   * @param obj Object
+   * @return boolean
+   */
+  @Override
+  public boolean equals(Object obj) {
+    if (obj instanceof ConsString){
+      var o = (ConsString) obj;
+      return o.getSType().equals(this.consString);
+    }
+    return false;
   }
 
   /**
@@ -42,56 +90,9 @@ public class ConsString implements Constant, IOperation {
    * @return Constant
    */
   @Override
-  public Constant addConsStr(Constant constant) {
-    SString result = this.consString.addSString((SString) constant.getSType());
+  public Constant addConsStr(ConsString constant) {
+    SString result = constant.getSType().add(this.consString);
     return new ConsString(result.toString());
   }
 
-  /**
-   * Add a Constant int
-   *
-   * @param constant Constant
-   * @return Constant
-   */
-  @Override
-  public Constant addConsInt(Constant constant) {
-    SString result = this.consString.addSInt((SInt) constant.getSType());
-    return new ConsString(result.toString());
-  }
-
-  /**
-   * Add a Constant float
-   *
-   * @param constant Constant
-   * @return Constant
-   */
-  @Override
-  public Constant addConsFloat(Constant constant) {
-    SString result = this.consString.addSFloat((SFloat) constant.getSType());
-    return new ConsString(result.toString());
-  }
-
-  /**
-   * Add a Constant binary
-   *
-   * @param constant Constant
-   * @return Constant
-   */
-  @Override
-  public Constant addConsBinary(Constant constant) {
-    SString result = this.consString.addSBinary((SBinary) constant.getSType());
-    return new ConsString(result.toString());
-  }
-
-  /**
-   * Add a Constant Bool
-   *
-   * @param constant Constant
-   * @return Constant
-   */
-  @Override
-  public Constant addConsBool(Constant constant) {
-    SString result = this.consString.addSBool((SBool) constant.getSType());
-    return new ConsString(result.toString());
-  }
 }
---
+++ src/test/java/cl/uchile/dcc/scrabble/model/AST/Constants/ConsStringTest.java
@@ -0,0 +1,94 @@
+package cl.uchile.dcc.scrabble.model.AST.Constants;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import cl.uchile.dcc.scrabble.model.Types.SString;
+import java.util.Random;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+
+class ConsStringTest {
+
+  private ConsString consString;
+  private String testString;
+  private int seed;
+  private Random rng;
+
+  @BeforeEach
+  void setUp() {
+    seed = new Random().nextInt();
+    rng = new Random(seed);
+    int strSize= rng.nextInt(50);
+    testString = RandomStringUtils.random(strSize, 0, Character.MAX_CODE_POINT,
+        true, true, null, rng);
+    consString = new ConsString(testString);
+  }
+
+  @RepeatedTest(50)
+  void constructorTest(){
+    ConsString expectedConsString = new ConsString(testString);
+    SString expectedSString = new SString(testString);
+
+    assertEquals(expectedConsString, consString, "ConsString don't match. Seed " + seed);
+    assertEquals(expectedConsString.hashCode(), consString.hashCode(), "Hashcode don't match. Seed " + seed);
+    assertEquals(expectedSString, consString.getSType());
+    assertEquals(consString, consString.eval());
+    assertNotEquals(consString, testString, "Objects match. Seed " + seed);
+
+    String differentString;
+
+    do {
+      differentString = RandomStringUtils.random(rng.nextInt(50), 0,
+          Character.MAX_CODE_POINT, true, true, null, rng);
+    } while (differentString.equals(testString));
+
+
+    ConsString differentConsString = new ConsString(differentString);
+    SString differentSString = new SString(differentString);
+    assertNotEquals(differentConsString, consString, "ConsString match. Seed " + seed);
+    assertNotEquals(differentSString, consString.getSType());
+    assertNotEquals(differentConsString, consString.eval());
+
+    assertEquals(testString, consString.toString(), "ConsString don't match. Seed " + seed);
+    assertNotEquals(differentString, consString.toString(), "ConsString match. Seed " + seed);
+  }
+
+  @Test
+  void eval() {
+  }
+
+  @RepeatedTest(50)
+  void addConsStrTest() {
+    String toAddString = RandomStringUtils.random(rng.nextInt(50), 0, Character.MAX_CODE_POINT,
+        true, true, null, rng);
+    ConsString toAddConsString = new ConsString(toAddString);
+
+    String expectedString = testString + toAddString;
+    ConsString expectedConsString = new ConsString(expectedString);
+
+    assertEquals(expectedConsString, consString.addConsStr(toAddConsString), "ConsString don't match. Seed " + seed);
+
+    String differentString;
+    do {
+      differentString = RandomStringUtils.random(rng.nextInt(50), 0,
+          Character.MAX_CODE_POINT, true, true, null, rng);
+    } while (differentString.equals(testString));
+
+    String differentToAddString;
+    do {
+      differentToAddString = RandomStringUtils.random(rng.nextInt(50), 0,
+          Character.MAX_CODE_POINT, true, true, null, rng);
+    } while (differentToAddString.equals(testString));
+
+    ConsString differentConsString = new ConsString(differentString + differentToAddString);
+
+    assertNotEquals(differentConsString, consString.addConsStr(toAddConsString), "ConsString match. Seed " + seed);
+  }
+
+  @Test
+  void add() {
+  }
+
+}
