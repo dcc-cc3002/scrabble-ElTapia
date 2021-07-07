0efe486f83b92dc2574a3525619620effe01431c
ElTapias <sebastian.tapia.st@gmail.com>
2021-07-07 11:57:25 -0400

    Add documentation

--- src/main/java/cl/uchile/dcc/scrabble/model/AST/IOperation.java
+++ src/main/java/cl/uchile/dcc/scrabble/model/AST/IOperation.java
@@ -3,5 +3,10 @@ package cl.uchile.dcc.scrabble.model.AST;
 import cl.uchile.dcc.scrabble.model.AST.Wrapper.*;
 
 public interface IOperation {
+
+  /**
+   * Eval an operation
+   * @return Constant
+   */
   Constant eval();
 }
