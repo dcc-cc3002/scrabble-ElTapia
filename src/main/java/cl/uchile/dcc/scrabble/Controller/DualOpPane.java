6a3ea25684e7021b3d639c15c1b3254f9abd0351
ElTapias <sebastian.tapia.st@gmail.com>
2021-07-30 11:26:23 -0400

    Minor fixes

--- src/main/java/cl/uchile/dcc/scrabble/Controller/DualOpPane.java
+++ src/main/java/cl/uchile/dcc/scrabble/Controller/DualOpPane.java
@@ -52,38 +52,38 @@ public class DualOpPane {
 
     addButton = new Button("+");
     addButton.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
-    addButton.setOnAction(e -> chosenOp += "Add");
+    addButton.setOnAction(e -> chosenOp = "Add");
     gridPane.add(addButton, 0, 0);
 
     subButton = new Button("-");
     subButton.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
-    subButton.setOnAction(e -> chosenOp += "Sub");
+    subButton.setOnAction(e -> chosenOp = "Sub");
     gridPane.add(subButton, 1, 0);
 
     timesButton = new Button("*");
     timesButton.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
-    timesButton.setOnAction(e -> chosenOp += "Times");
+    timesButton.setOnAction(e -> chosenOp = "Times");
     gridPane.add(timesButton, 2, 0);
 
     divideButton = new Button("÷");
     divideButton.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
-    divideButton.setOnAction(e -> chosenOp += "Divide");
+    divideButton.setOnAction(e -> chosenOp = "Divide");
     gridPane.add(divideButton, 0, 1);
 
     orButton = new Button("∨");
     orButton.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
-    orButton.setOnAction(e -> chosenOp += "Or");
+    orButton.setOnAction(e -> chosenOp = "Or");
     gridPane.add(orButton, 1, 1);
 
     andButton = new Button("∧");
     andButton.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
-    andButton.setOnAction(e -> chosenOp += "And");
+    andButton.setOnAction(e -> chosenOp = "And");
     gridPane.add(andButton, 2, 1);
 
     return gridPane;
   }
 
-  public static String getChooseOp(){
+  public static String getChosenOp(){
     return chosenOp;
   }
 }
