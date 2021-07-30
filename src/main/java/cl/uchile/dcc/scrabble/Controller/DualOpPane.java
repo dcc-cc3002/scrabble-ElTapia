7fefe76c514c590a2ebe8c853b8e28dd403b8cbe
ElTapias <sebastian.tapia.st@gmail.com>
2021-07-30 11:06:03 -0400

    Create dual operations pane

---
+++ src/main/java/cl/uchile/dcc/scrabble/Controller/DualOpPane.java
@@ -0,0 +1,89 @@
+package cl.uchile.dcc.scrabble.Controller;
+
+import cl.uchile.dcc.scrabble.model.AST.IComponent;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import javafx.scene.control.Button;
+
+public class DualOpPane {
+  static Button addButton;
+  static Button subButton;
+  static Button timesButton;
+  static Button divideButton;
+  static Button orButton;
+  static Button andButton;
+
+  static GridPane gridPane;
+  static String chosenOp;
+
+  public static GridPane getGridPane(){
+
+    gridPane = new GridPane();
+    gridPane.setPadding(new Insets(10));
+    gridPane.setVgap(8);
+    gridPane.setHgap(10);
+
+    ColumnConstraints column1 = new ColumnConstraints();
+    column1.setPercentWidth(33);
+    ColumnConstraints column2 = new ColumnConstraints();
+    column2.setPercentWidth(33);
+    ColumnConstraints column3 = new ColumnConstraints();
+    column3.setPercentWidth(34);
+
+    RowConstraints row1 = new RowConstraints();
+    row1.setPercentHeight(50);
+    RowConstraints row2 = new RowConstraints();
+    row2.setPercentHeight(50);
+
+    gridPane.getColumnConstraints().addAll(column1, column2, column3);
+    gridPane.getRowConstraints().addAll(row1, row2);
+
+    addButton = new Button("+");
+    addButton.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+    addButton.setOnAction(e -> chosenOp += "Add");
+    gridPane.add(addButton, 0, 0);
+
+    subButton = new Button("-");
+    subButton.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+    subButton.setOnAction(e -> chosenOp += "Sub");
+    gridPane.add(subButton, 1, 0);
+
+    timesButton = new Button("*");
+    timesButton.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+    timesButton.setOnAction(e -> chosenOp += "Times");
+    gridPane.add(timesButton, 2, 0);
+
+    divideButton = new Button("÷");
+    divideButton.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+    divideButton.setOnAction(e -> chosenOp += "Divide");
+    gridPane.add(divideButton, 0, 1);
+
+    orButton = new Button("∨");
+    orButton.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+    orButton.setOnAction(e -> chosenOp += "Or");
+    gridPane.add(orButton, 1, 1);
+
+    andButton = new Button("∧");
+    andButton.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+    andButton.setOnAction(e -> chosenOp += "And");
+    gridPane.add(andButton, 2, 1);
+
+    return gridPane;
+  }
+
+  public static String getChooseOp(){
+    return chosenOp;
+  }
+}
--- src/main/java/cl/uchile/dcc/scrabble/view/App.java
+++ src/main/java/cl/uchile/dcc/scrabble/view/App.java
@@ -1,10 +1,15 @@
 package cl.uchile.dcc.scrabble.view;
 
+import cl.uchile.dcc.scrabble.Controller.DualOpPane;
+import cl.uchile.dcc.scrabble.Controller.NumbersPane;
 import javafx.application.Application;
 import javafx.beans.property.StringProperty;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -42,11 +47,31 @@ public class App extends Application {
             System.out.println(calcResult.getValue());
         });
 
+        GridPane completeLayout = new GridPane();
+
+        ColumnConstraints column1 = new ColumnConstraints();
+        column1.setPercentWidth(10);
+        ColumnConstraints column2 = new ColumnConstraints();
+        column2.setPercentWidth(80);
+        ColumnConstraints column3 = new ColumnConstraints();
+        column3.setPercentWidth(10);
+
+        completeLayout.getColumnConstraints().addAll(column1, column2, column3);
+
+        RowConstraints row1 = new RowConstraints();
+        row1.setPercentHeight(70);
+        RowConstraints row2 = new RowConstraints();
+        row2.setPercentHeight(30);
+        completeLayout.getRowConstraints().addAll(row1, row2);
+
+        GridPane dualOp = DualOpPane.getGridPane();
+        completeLayout.add(dualOp, 1, 1);
+
         VBox layout = new VBox(10);
         layout.setPadding(new Insets(20));
         layout.getChildren().addAll(intButton, binaryButton, floatButton);
 
-        Scene scene = new Scene(layout, 500, 500);
+        Scene scene = new Scene(completeLayout, 500, 500);
         window.setScene(scene);
         window.show();
     }
